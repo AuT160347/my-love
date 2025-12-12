e2ca0977af010c9b7228bc97c5f3f939697c660a
AuT160347 <atnuntawat@gmail.com>
2025-12-12 18:53:24 +0700

    aaa

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,24 @@ import React, { useState } from "react";
 import ImageSliderModal from "./ImageSliderModal";
 import "./App.css";
 
+// import รูป
+import i1 from "./img/i1.jpg";
+import i2 from "./img/i2.jpg";
+import i3 from "./img/i3.jpg";
+import i4 from "./img/i4.jpg";
+import i5 from "./img/i5.jpg";
+import i6 from "./img/i6.jpg";
+import i7 from "./img/i7.jpg";
+import i8 from "./img/i8.jpg";
+import i9 from "./img/i9.jpg";
+import i10 from "./img/i10.jpg";
+
 export default function App() {
   const [open, setOpen] = useState(false);
 
   const images = [
-    "/img/i1.jpg",
-    "/img/i2.jpg",
-    "/img/i3.jpg",
-    "/img/i4.jpg",
-    "/img/i5.jpg",
-    "/img/i6.jpg",
-    "/img/i7.jpg",
-    "/img/i8.jpg",
-    "/img/i9.jpg",
-    "/img/i10.jpg",
+    i1, i2, i3, i4, i5,
+    i6, i7, i8, i9, i10
   ];
 
   return (
